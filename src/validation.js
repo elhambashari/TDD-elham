0b4f376f378c63f94664d0745040fe58bc397ae3
Elham Bashari <elhambashari84@gmail.com>
2025-05-23 17:53:40 +0200

    npm test isproduct green

--- src/validation.js
+++ src/validation.js
@@ -21,7 +21,11 @@ function isCartItem(obj) {
 
 
 function isProduct(obj) {
-	return
+	return typeof obj==="object" && obj!== null &&
+	 'id' in obj &&
+	 typeof obj.id === 'number' && 'name' in obj &&
+	  typeof obj.name === 'string'&& 'price'in obj &&
+	   typeof obj.price==='number';
 }
 
 
