2ad832cea94f7f9194e823e260f0be277928a4d5
Elham Bashari <elhambashari84@gmail.com>
2025-05-24 09:35:34 +0200

    refaktor på cart.js

--- src/cart.js
+++ src/cart.js
@@ -1,21 +1,4 @@
-/*
-Din uppgift:
-- skriv testfall för alla funktionerna nedan i cart.test.js (RED)
-- skriv kod här för att implementera funktionerna (GREEN)
 
-Tips:
-- börja med att identifiera VAD som ska testas.
-- om du testar t.ex. removeFromCart får du använda addToCart i början av testet. Den kommer nämligen ha sina egna tester
-
-*/
-// function getCartItemCount()
-// function getItem(index)
-// function getTotalCartValue()
-// function addToCart(newItem)
-// function removeFromCart(itemId)
-// function editCart(itemId, newValues)
-// function clearCart()
-// -------------------------------------------------- //
 
 import { isCartItem, isProduct } from "./validation.js"
 
@@ -25,7 +8,7 @@ let idCounter = 2002
 
 
 // Din kod börjar här
-// Du får en funktion att börja med
+
 
 function getCartItemCount() {
 	return cart.reduce((total,item)=>total+item.amount,0)
